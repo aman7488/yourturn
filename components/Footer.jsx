0b5cd65a38be926240413d01e11c11b4a6101093
Aman Kumar <aman@Amans-MacBook-Air.local>
2025-05-24 22:59:03 +0530

    updated logo

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer>
       <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 gap-10 py-14 border-b border-gray-500/30 text-gray-500">
         <div className="w-4/5">
-          <Image className="w-28 md:w-32" src={assets.logo} alt="logo" />
+          <Image className="cursor-pointer inline-block align-middle" width={100} src={assets.logo} alt="logo" />
           <p className="mt-6 text-sm font-medium">
           Asquare is an inclusive, family-oriented lifestyle e-commerce brand offering high-quality, everyday essentials for men, women, and kids. From wardrobe basics to thoughtful accessories, the brand celebrates the small things that bring comfort, confidence, and connection.
           </p>
